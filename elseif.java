eb587f849aba493da648362dc33c114d60ae1f6f
RazaqCode <razaqrealm@gmail.com>
2025-05-19 13:48:08 +0530

    taxCalc

--- elseif.java
+++ elseif.java
@@ -1,17 +1,20 @@
 import java.util.*;
 public class elseif{
-    public static void main (String [] args){
-        int age = 1;
+    public static void main (String[]args){
+        Scanner sc = new Scanner (System.in);
+        int income = sc.nextInt();
+        int tax;
 
-        if (age >= 18){
-            System.out.println("adult ");
+        if (income <500000 ){
+            tax = 0;
         }
-        else if (age >= 13 && age < 18) {
-            System.out.println("teen");
-        }
-        else{
-            System.out.println(" child");
-        }
+        else if (income > 500000 && income < 1000000){
+            tax = (int) (income  * 0.2);
 
+        }
+        else {
+            tax = (int) (income  * 0.3);
+        }
+        System.out.println("your tax is :" + tax );
     }
 }
