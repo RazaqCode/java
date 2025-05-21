c632ea9b053de6f927185cbe20d1d4d0708c42e2
RazaqCode <razaqrealm@gmail.com>
2025-05-21 21:35:06 +0530

    odd

--- elseif.java
+++ elseif.java
@@ -1,7 +1,7 @@
 import java.util.*;
 public class elseif{
     public static void main (String[]args){
-        int number  = 4;
+        int number  = 9;
         String type  = (number %2 ==0 ) ? "even ":"odd";
 
         System.out.println(type);
