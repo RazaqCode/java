4ed2422908357eb01fe216068b97364b8480836e
RazaqCode <razaqrealm@gmail.com>
2025-05-22 21:31:09 +0530

    year

--- elseif.java
+++ elseif.java
@@ -1,15 +1,20 @@
 import java.util.*;
 public class elseif{
     public static void main (String[]args){
-       double temp = 99;
+       Scanner sc = new Scanner(System.in);
 
-       if(temp >100){
-        System.out.println("fever");
-    
-       }
-       else {
-        System.out.println(" no fever");
-       }
+        System.out.print("Input the year: ");
+        int year = sc.nextInt();
+
+        boolean x = (year % 4) == 0;
+        boolean y = (year % 100) != 0;
+        boolean z = ((year % 100 == 0) && (year % 400 == 0));
+
+        if (x && (y || z)) {
+            System.out.println(year + " is a leap year");
+        } else {
+            System.out.println(year + " is not a leap year");
+        }
        
     }
 } 
