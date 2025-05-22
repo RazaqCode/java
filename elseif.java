b64fe88028fa9c69e8d6dd00d43e559f069734d7
RazaqCode <razaqrealm@gmail.com>
2025-05-22 19:47:17 +0530

    no fever

--- elseif.java
+++ elseif.java
@@ -1,27 +1,15 @@
 import java.util.*;
 public class elseif{
     public static void main (String[]args){
-       Scanner sc  = new Scanner (System.in);
-       //int a = sc.nextInt(); 
-        System.out.println("enter a number :"); 
-       char operator = sc.next().charAt(0);
+       double temp = 99;
 
-       switch (operator){
-        case '1' :System.out.println("mon");
-        break;
-
-        case '2' :System.out.println("tue");
-        break;
-
-        case '3' :System.out.println("wed");
-        break;
-
-        case '4' :System.out.println("thu");
-        break;
-
-        default:System.out.println("good day");
+       if(temp >100){
+        System.out.println("fever");
+    
        }
-
-
+       else {
+        System.out.println(" no fever");
+       }
+       
     }
 } 
