2535483c7d1c48f6580a2b9e921df51cdb57b2f2
RazaqCode <razaqrealm@gmail.com>
2025-05-24 23:28:14 +0530

    star while

--- loops.java
+++ loops.java
@@ -1,9 +1,14 @@
 import java.util.*;
 public class loops {
     public static void main (String args []){
-        for (int  i = 1; i<=4; i++){
-            System.out.println("* * * *");
+        //for (int  i = 1; i<=4; i++){
+        //  System.out.println("* * * *");
 
+        //}
+        int i =1;
+        while (i<=4){
+            System.out.println("* * * *");
+            i ++;
         }
       
 
