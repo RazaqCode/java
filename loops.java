af9dbd0a84b9d8b3fbf8169af78db0f1fd3030e5
RazaqCode <razaqrealm@gmail.com>
2025-05-24 22:45:28 +0530

    for loop

--- loops.java
+++ loops.java
@@ -1,17 +1,11 @@
 import java.util.*;
 public class loops {
     public static void main (String args []){
-        Scanner sc = new Scanner (System.in);
-        int n = sc.nextInt();
-        int sum = 0;
-        int i = 1;
-        while (i<=n){
-            sum = sum + i;
-            
-            i++;
+        for (int  i = 0; i<=10; i++){
+            System.out.println("i ="+ i);
 
         }
-        System.out.println(sum);
+      
 
         }
 
