3bc5f61dc9d5ca3b6001c7a9ac84b2d7fd6d0f9e
RazaqCode <razaqrealm@gmail.com>
2025-05-27 00:00:39 +0530

    sum

--- loops.java
+++ loops.java
@@ -3,19 +3,24 @@ public class loops {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // Ask the user for a number
+        // Ask user for input
         System.out.print("Enter a number: ");
-        int number = scanner.nextInt();
+        int n = scanner.nextInt();
 
-        // Print the multiplication table from 1 to 10
-        System.out.println("Multiplication table for " + number + ":");
-        for (int i = 1; i <= 10; i++) {
-            System.out.println(number + " x " + i + " = " + (number * i));
+        int sum = 0;
+
+        // Loop from 1 to n
+        for (int i = 1; i <= n; i++) {
+            if (i % 2 == 0) {
+                sum = sum + i;
+            }
         }
+
+        // Print the result
+        System.out.println("Sum of even numbers from 1 to " + n + " is: " + sum);
 
         scanner.close();
     }
-
     }
     
 
