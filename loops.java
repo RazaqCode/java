2d694555d0b6ea3e5b4bdf12f938a54f6043a5ba
RazaqCode <razaqrealm@gmail.com>
2025-05-26 23:53:32 +0530

    table

--- loops.java
+++ loops.java
@@ -1,18 +1,20 @@
 import java.util.*;
 public class loops {
-    public static void main (String args []){
-        //for (int  i = 1; i<=4; i++){
-        //  System.out.println("* * * *");
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
-        //}
-        int i =1;
-        while (i<=4){
-            System.out.println("* * * *");
-            i ++;
-        }
-      
+        // Ask the user for a number
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
 
+        // Print the multiplication table from 1 to 10
+        System.out.println("Multiplication table for " + number + ":");
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(number + " x " + i + " = " + (number * i));
         }
+
+        scanner.close();
+    }
 
     }
     
