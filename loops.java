8c83bb59552b0f61706cfa9e5c764fe2045f276e
RazaqCode <razaqrealm@gmail.com>
2025-05-30 21:03:42 +0530

    var add

--- loops.java
+++ loops.java
@@ -1,25 +1,9 @@
 import java.util.*;
 public class loops {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        // Ask user for input
-        System.out.print("Enter a number: ");
-        int n = scanner.nextInt();
-
-        int sum = 0;
-
-        // Loop from 1 to n
-        for (int i = 1; i <= n; i++) {
-            if (i % 2 == 0) {
-                sum = sum + i;
-            }
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
         }
-
-        // Print the result
-        System.out.println("Sum of even numbers from 1 to " + n + " is: " + sum);
-
-        scanner.close();
     }
     }
     
