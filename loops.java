5f296dbb3c34893cad465bb20f84d54cc22c65d9
RazaqCode <razaqrealm@gmail.com>
2025-06-05 11:20:18 +0530

    lastDigit

--- loops.java
+++ loops.java
@@ -1,9 +1,15 @@
 import java.util.*;
 public class loops {
     public static void main(String[] args) {
-        for (int i = 1; i <= 10; i++) {
-            System.out.println(i);
+        int n = 10899;
+
+        while (n > 0 ){
+            int lastDigit = n % 10;
+            System.out.print( lastDigit + "");
+            n = n / 10;
+
         }
+        //System.out.println( );
     }
     }
     
