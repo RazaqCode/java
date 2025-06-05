63d0fe476b737004c16bc19209d1df962ab0fd3f
RazaqCode <razaqrealm@gmail.com>
2025-06-05 16:09:33 +0530

    reverse

--- loops.java
+++ loops.java
@@ -2,15 +2,22 @@ import java.util.*;
 public class loops {
     public static void main(String[] args) {
         int n = 10899;
-
-        while (n > 0 ){
+        int rev = 0;
+    
+        while(n >0){
             int lastDigit = n % 10;
-            System.out.print( lastDigit + "");
-            n = n / 10;
+            rev = (rev*10) + lastDigit;
+            n = n/10;
+
 
         }
-        //System.out.println( );
+        System.out.println( rev);
+
+    
+    
+        }
+        
     }
-    }
+    
     
 
