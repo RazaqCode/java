e1b56ed93c19a5b9462985c32da27739003091e0
RazaqCode <razaqrealm@gmail.com>
2025-06-21 22:44:58 +0530

    isPrime

--- loops.java
+++ loops.java
@@ -1,12 +1,30 @@
+import java.util.Scanner;
 public class loops {
     public static void main (String [] args){
-        String input = "Hello, World!";
+        Scanner scanner = new Scanner(System.in);
         
-        // Method 1: Using StringBuilder
-        String reversed = new StringBuilder(input).reverse().toString();
+        System.out.print("Enter a number: ");
+        int num = scanner.nextInt();
         
-        System.out.println("Original: " + input);
-        System.out.println("Reversed: " + reversed);
+        boolean isPrime = true;
+
+        if (num <= 1) {
+            isPrime = false;
+        } else {
+            // Check from 2 to sqrt(num)
+            for (int i = 2; i <= Math.sqrt(num); i++) {
+                if (num % i == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+        }
+
+        if (isPrime)
+            System.out.println(num + " is a Prime Number.");
+        else
+            System.out.println(num + " is NOT a Prime Number.");
     }
 }
+
 
