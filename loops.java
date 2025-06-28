ce79d7f077221e2436f51b8a969fdc8cc2f7bdea
RazaqCode <razaqrealm@gmail.com>
2025-06-28 20:54:08 +0530

    num2Method

--- loops.java
+++ loops.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 public class loops {
     public static void main (String [] args){
-        for (int i = 1; i <= 5;i++){
+        for (int i = 1; i <5;i++){
             System.out.println(i);
         }
     }
