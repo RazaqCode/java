5872c97831a3c1b63ae47966b20a6b47275a5db1
RazaqCode <razaqrealm@gmail.com>
2025-07-24 20:29:12 +0530

    rightalin

--- loops.java
+++ loops.java
@@ -1,10 +1,14 @@
-
 public class loops {
     public static void main (String [] args){
         int n = 4;
-        for (int line=1; line<=n; line++){
-            for(int star=1; star<=(n-line+1); star++){
-            System.out.print("*");
+        for (int line = 1; line <= n; line++){
+            // Print spaces
+            for (int space = 1; space <= n - line; space++){
+                System.out.print(" ");
+            }
+            // Print stars
+            for(int star = 1; star <= line; star++){
+                System.out.print("*");
             }
             System.out.println();
         }
