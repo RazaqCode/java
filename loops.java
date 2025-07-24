d32802cc0e432ce2f034b0bde43686be2516781f
RazaqCode <razaqrealm@gmail.com>
2025-07-24 20:31:04 +0530

    chA

--- loops.java
+++ loops.java
@@ -1,9 +1,11 @@
 public class loops {
     public static void main (String [] args){
         int n = 4;
+        char ch = 'A'; // Starting character
         for (int line = 1; line <= n; line++){
-            for(int number = 1; number <= (n - line + 1); number++){
-                System.out.print(number + " ");
+            for(int characterCount = 1; characterCount <= line; characterCount++){
+                System.out.print(ch + " ");
+                ch++; // Increment character for the next print
             }
             System.out.println();
         }
