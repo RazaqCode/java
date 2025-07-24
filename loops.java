877efd8e7f4965ee091ca38b2a3c1b65b7346b25
RazaqCode <razaqrealm@gmail.com>
2025-07-24 20:33:45 +0530

    manja

--- loops.java
+++ loops.java
@@ -1,17 +1,6 @@
 public class loops {
     public static void main (String [] args){
-        int rows = 5; // You can adjust this for the number of rows
-        int cols = 7; // You can adjust this for the number of columns
-
-        for (int i = 1; i <= rows; i++) {
-            for (int j = 1; j <= cols; j++) {
-                if (i == 1 || i == rows || j == 1 || j == cols) {
-                    System.out.print("*");
-                } else {
-                    System.out.print(" ");
-                }
-            }
-            System.out.println();
+        
+            System.out.println("this is java man");
         }
     }
-}
