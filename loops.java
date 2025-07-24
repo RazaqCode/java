fec255c27b609d482531f9d91672d13732680db2
RazaqCode <razaqrealm@gmail.com>
2025-07-24 20:30:06 +0530

    rowright

--- loops.java
+++ loops.java
@@ -2,13 +2,8 @@ public class loops {
     public static void main (String [] args){
         int n = 4;
         for (int line = 1; line <= n; line++){
-            // Print spaces
-            for (int space = 1; space < line; space++){
-                System.out.print(" ");
-            }
-            // Print stars
-            for(int star = 1; star <= (n - line + 1); star++){
-                System.out.print("*");
+            for(int number = 1; number <= line; number++){
+                System.out.print(number + " "); // Added a space for better readability
             }
             System.out.println();
         }
