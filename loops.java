b881add462e3bc89f71b2069cb94189035ec462a
RazaqCode <razaqrealm@gmail.com>
2025-07-24 20:29:42 +0530

    halfright

--- loops.java
+++ loops.java
@@ -3,11 +3,11 @@ public class loops {
         int n = 4;
         for (int line = 1; line <= n; line++){
             // Print spaces
-            for (int space = 1; space <= n - line; space++){
+            for (int space = 1; space < line; space++){
                 System.out.print(" ");
             }
             // Print stars
-            for(int star = 1; star <= line; star++){
+            for(int star = 1; star <= (n - line + 1); star++){
                 System.out.print("*");
             }
             System.out.println();
