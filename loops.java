be8b8e8d5bae07d08629108ba2ccecc2b7a6b29d
RazaqCode <razaqrealm@gmail.com>
2025-07-24 20:30:42 +0530

    rowthat

--- loops.java
+++ loops.java
@@ -2,8 +2,8 @@ public class loops {
     public static void main (String [] args){
         int n = 4;
         for (int line = 1; line <= n; line++){
-            for(int number = 1; number <= line; number++){
-                System.out.print(number + " "); // Added a space for better readability
+            for(int number = 1; number <= (n - line + 1); number++){
+                System.out.print(number + " ");
             }
             System.out.println();
         }
